c6678830297cc20dc79354dd4e5eae78822ff596
Rui Lin <rui_lin@live.ca>
2014-02-10 16:41:52 +0000

    changed careers page

---
+++ main-www/careers.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Ulticoder!</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Average' rel='stylesheet' type='text/css'>
+	<script src="jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+</head>
+<body>
+
+<style type="text/css">
+
+.youtubeVideo{
+	width: 580px !important; 
+	height: 390px !important; 
+}
+
+</style>
+
+<script type="text/javascript">
+	
+//Tracking Code Customizations Only
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-47503831-1']);
+_gaq.push(['_setCookiePath', '/careers.php']);
+_gaq.push(['_trackPageview']); 
+
+</script>
+
+
+
+<div id="container">
+    
+	<?php include("topnav.html"); ?>
+<fieldset>
+<legend>Careers</legend>   
+<div class="content">
+    
+
+	<table cellpadding="0" cellspacing="0" align="Left" border="0" width="100%" bordercolor="#FFFFFF">
+	<tr>
+		<td align="Center" class="Cell"><img id="_ctl7" title="" src="https://myrec.ultimatesoftware.com/usg1006/JobBoard/Custom/SkinCustom/USG1006/bestcomp.jpg" border="0" /></td>
+	</tr>
+	<tr>
+		<td align="Center" class="Cell">
+		<iframe title="Ultimate Software" class="youtubeVideo" type="text/html" src="https://www.youtube.com/embed/qUDmnT5qeNw?autoplay=0"frameborder="0" allowFullScreen></iframe>
+		</td>
+	</tr>
+	</table>
+
+	<div>
+	<h4>Opportunities:</h4>
+	<ul>
+		<li><a href="https://myrec.ultimatesoftware.com/usg1006/JobBoard/JobDetails.aspx?__ID=*41C69C9DCC429218" target="_break">Senior C# Software Engineer in Florida</a></li>
+		<li><a href="https://myrec.ultimatesoftware.com/usg1006/JobBoard/JobDetails.aspx?__ID=*F8D5B36530D340E7" target="_break">Software Engineer Intern in Florida</a></li>
+		<li><a href="https://myrec.ultimatesoftware.com/usg1006/JobBoard/JobDetails.aspx?__ID=*D4FF620DFFC66D4E" target="_break">Software Engineer Intern in Toronto</a></li>
+	</ul>
+	</div>
+
+	<span class="Message">Having Technical issues? Please contact us at <a href="mailto:Careers@ultimatesoftware.com">Careers@ultimatesoftware.com</a> with your issue and contact information so that we may assist you. </span>
+
+</div>
+</fieldset>
+</div>
+
+</body>
+</html>
