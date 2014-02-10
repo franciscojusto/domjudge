4fcb8b7dfea570201e41cffdf28baf37b958965c
Rui Lin <rui_lin@live.ca>
2014-02-10 16:27:15 +0000

    add changes to the careers page

--- main-www/index.php
+++ main-www/index.php
@@ -32,9 +32,9 @@
 
     <div class="content">
     <div class="left" style="overflow:hidden">
-<!--[if IE]><object width="100%" height="410" id="viddlerOuter-d0c49de9" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"><param name="movie" value="//www.viddler.com/player/d0c49de9/"><param name="allowScriptAccess" value="always"><param name="allowNetworking" value="all"><param name="allowFullScreen" value="true"><param name="flashVars" value="f=1&autoplay=f&disablebranding=1&loop=0&nologo=1&hd=1&nobuttons=t"><object id="viddlerInner-d0c49de9"><video id="viddlerVideo-d0c49de9" src="//www.viddler.com/file/d0c49de9/html5mobile/" type="video/mp4" width="100%" height="380" poster="//www.viddler.com/thumbnail/d0c49de9/" controls="controls" x-webkit-airplay="allow"></video></object></object><![endif]-->
+<!--[if IE]><object width="100%" height="510" id="viddlerOuter-d0c49de9" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"><param name="movie" value="//www.viddler.com/player/d0c49de9/"><param name="allowScriptAccess" value="always"><param name="allowNetworking" value="all"><param name="allowFullScreen" value="true"><param name="flashVars" value="f=1&autoplay=f&disablebranding=1&loop=0&nologo=1&hd=1&nobuttons=t"><object id="viddlerInner-d0c49de9"><video id="viddlerVideo-d0c49de9" src="//www.viddler.com/file/d0c49de9/html5mobile/" type="video/mp4" width="100%" height="480" poster="//www.viddler.com/thumbnail/d0c49de9/" controls="controls" x-webkit-airplay="allow"></video></object></object><![endif]-->
 <!--[if !IE]> <!-->
-<object width="100%" height="410" id="viddlerOuter-d0c49de9" type="application/x-shockwave-flash" data="//www.viddler.com/player/d0c49de9/"> <param name="movie" value="//www.viddler.com/player/d0c49de9/"> <param name="allowScriptAccess" value="always"><param name="allowNetworking" value="all"><param name="allowFullScreen" value="true"><param name="flashVars" value="f=1&amp;autoplay=f&amp;disablebranding=1&amp;loop=0&amp;nologo=1&amp;hd=1&amp;nobuttons=t"><object id="viddlerInner-d0c49de9"> <video id="viddlerVideo-d0c49de9" src="//www.viddler.com/file/d0c49de9/html5mobile/" type="video/mp4" height="380" poster="//www.viddler.com/thumbnail/d0c49de9/" controls="controls" x-webkit-airplay="allow"></video> </object></object>
+<object width="100%" height="510" id="viddlerOuter-d0c49de9" type="application/x-shockwave-flash" data="//www.viddler.com/player/d0c49de9/"> <param name="movie" value="//www.viddler.com/player/d0c49de9/"> <param name="allowScriptAccess" value="always"><param name="allowNetworking" value="all"><param name="allowFullScreen" value="true"><param name="flashVars" value="f=1&amp;autoplay=f&amp;disablebranding=1&amp;loop=0&amp;nologo=1&amp;hd=1&amp;nobuttons=t"><object id="viddlerInner-d0c49de9"> <video id="viddlerVideo-d0c49de9" src="//www.viddler.com/file/d0c49de9/html5mobile/" type="video/mp4" height="480" poster="//www.viddler.com/thumbnail/d0c49de9/" controls="controls" x-webkit-airplay="allow"></video> </object></object>
 <!--<![endif]-->
     </div>
     <div class="right">
@@ -42,8 +42,10 @@
         <form class="flat-button" action="register.php">
         <input type="submit" value="Register" class="flat-button">
         </form>
-        <div class="label">Our next competition is February 13, 5:30pm - 8:30pm</div>
+        <div class="label">Our next competition is March 8 2014, 1-5pm</div>
         </div>
+	<p>Join our March 8 coding competition for a chance to win an all inclusive trip to Miami South Beach and an exclusive tour of Ultimate Software HQ.
+	</p>
         <h4>What is UltiCoder?</h4>
         <p><em>UltiCoder</em> brings together bright minds to solve tough coding problems and provide the best solutions in a competition awarding prizes to top performers.</p>
         <h4>Who are we?</h4>
