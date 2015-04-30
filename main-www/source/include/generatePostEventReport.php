d0f7e09d4076e2835cb5c8404f91aa0878c3b79e
Chuang L <mail@ulticoder.com>
2015-04-30 17:01:56 +0000

    added post event report generator

--- main-www/source/include/generatePostEventReport.php
+++ main-www/source/include/generatePostEventReport.php
@@ -55,26 +55,31 @@ class PostEventReport {
         $output .= '<th>Email</th>';
 		$output .= '<th>Location</th></tr></thead><tbody>';
 		
-        $query = "SELECT user.name, user.email, user.ip_address FROM user INNER JOIN $this->rankTable ON user.teamid=$this->rankTable.teamid ";
+        $query = "SELECT user.username, user.name, user.email, user.last_ip_address FROM user INNER JOIN $this->rankTable ON user.teamid=$this->rankTable.teamid ";
 		$query .= "WHERE $this->rankTable.cid=$this->contestId ORDER BY $this->rankTable.correct DESC, $this->rankTable.totaltime LIMIT $this->topContestants;";
 		
 		$result = mysql_query($query, $this->connection);
 		$rank = 1;
 		while($row = mysql_fetch_array($result))
 		{
-			$teamid = $row['name'];
-			$email = $row['email'];
-            // Get location from ip using GeoPlugin API
-            $ip = $row['ip_address'];
-            $response = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip));
-            if ($response)
-            {
-                $city = $response['geoplugin_city'].', ';
-                $region = $response['geoplugin_regionName'].', ';
-                $country = $response['geoplugin_countryName'];
-            }
-			$output .= '<tr><td>'.$rank.'</td><td>'.$teamid.'</td><td>'.$email.'</td><td>'.$city.$region.$country.'</tr>';
-			$rank++;
+		    $teamid = $row['name'];
+		    $email = $row['email'];
+            	    /* Get location from ip using GeoPlugin API
+            	    $ip = $row['last_ip_address'];
+            	    $response = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip));
+            	    if ($response)
+		    {
+                	$city = $response['geoplugin_city'].', ';
+                 	$region = $response['geoplugin_regionName'].', ';
+                	$country = $response['geoplugin_countryName'];
+            	    }
+		    */
+		    $username = $row['username'];
+		    $addressResult = mysql_query("SELECT addressln1, addressln2, zipcode FROM user_info WHERE username='$username';", $this->connection);
+		    $addressRow = mysql_fetch_row($addressResult);
+		    $address = $addressRow[0].' '.$addressRow[1].' '.$addressRow[2];
+		    $output .= '<tr><td>'.$rank.'</td><td>'.$teamid.'</td><td>'.$email.'</td><td>'.$address.'</tr>';
+		    $rank++;
 		}
 		$output .= '</tbody></table>';
 		return $output;
@@ -91,25 +96,38 @@ class PostEventReport {
         $output .= '<th>Name</th>';
         $output .= '<th>Email</th>';
         $output .= '<th>Number of <br>Completed Problem</th>';
-        $output .= '<th>Completed Problems</th></tr></thead><tbody>';
+        $output .= '<th>Completed Problems</th>';
+	$output .= '<th>Address</th>';
+	$output .= '<th>T-Shirt Size</th></tr></thead><tbody>';
 
-        $getUsersQuery = "SELECT user.name, user.email from user INNER JOIN  $this->rankTable on user.teamid=$this->rankTable.teamid ";
+        $getUsersQuery = "SELECT user.username, user.name, user.email, user.teamid from user INNER JOIN  $this->rankTable on user.teamid=$this->rankTable.teamid ";
         $getUsersQuery .= "WHERE $this->rankTable.cid=$this->contestId AND $this->rankTable.correct>=1;";
-       
-        while ($user = mysql_fetch_array(mysql_query($getUsersQuery, $this->connection)))
+       	
+	$result = mysql_query($getUsersQuery, $this->connection);
+        while ($user = mysql_fetch_array($result))
         {
-            $name = $row['name'];
-            $email = $row['email'];
+	    $username = $user['username'];
+            $name = $user['name'];
+            $email = $user['email'];
+	    $teamid = $user['teamid'];
             
-            $getCompletedProblemsQuery = "SELECT probid FROM $this->scoreTable WHERE cid=$this->contestId AND teamid=$name AND is_correct=1;";
-            $completedProblems = mysql_fetch_array(mysql_query($getCompletedProblemsQuery, $this->connection));
+            $getCompletedProblemsQuery = "SELECT probid FROM $this->scoreTable WHERE cid=$this->contestId AND teamid='$teamid' AND is_correct=1;";
+            $completedProblems = mysql_query($getCompletedProblemsQuery, $this->connection);
             
-            $output .= "<tr><td>$name</td><td>$email</td><td>".count($completedProblems)."</td><td>";
-            foreach($completedProblems as $problem) {
-                $output .= $problem.', ';
-            }
-            $output .= '</td></tr>';
-        }       
+            $output .= "<tr><td>$name</td><td>$email</td><td>".mysql_num_rows($completedProblems)."</td><td>";
+            while ($problem = mysql_fetch_array($completedProblems)) 
+	    {
+                $output .= $problem['probid'].', ';
+            } 
+            $output .= '</td><td>';
+
+	    // Get Address
+	    $addressResult = mysql_query("SELECT addressln1, addressln2, zipcode, tshirtsize FROM user_info WHERE username='$username';", $this->connection);
+   	    $userInfo = mysql_fetch_row($addressResult);
+	    $address = $userInfo[0].' '.$userInfo[1].' '.$userInfo[2];	    
+	    $tshirtsize = $userInfo[3];
+	    $output .= $address.'</td><td>'.$tshirtsize.'</td></tr>';
+        }        
         $output .= '</tbody></table>';
         return $output;
     }
